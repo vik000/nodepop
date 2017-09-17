01d478d2e89886ad5714e329a14be8c3aa6f86be
Victor Manuel García Sanabria <vik000@gmail.com>
2017-09-17 11:21:20 +0200

    copia de seguridad, still setting up

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 //Connect to Mongoose:
-mongoose.connect('');
+require('./lib/connectMongoose');
+// mongoose.connect('mongodb://localhost/nodepop');
+// var db=mongoose.connection;
+
+app.get('/',(req,res)=>{
+  res.send('Hello World');
+});
+
+app.listen(3000);
+console.log('running on port 3000');
